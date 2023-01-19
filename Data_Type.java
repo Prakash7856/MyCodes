a0b668d6ae44f73397f2f462b8897d6121a26d03
Prakash <prakashpes27@gmail.com>
2023-01-19 14:43:39 +0530

    Added code

---
+++ Data_Type.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        System.out.printf("%.7f %.3f",result,result);
+    }    
+    
+}
